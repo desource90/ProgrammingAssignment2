5f868deb5af7392b31d9567fcdaa37f4ac0cf73f
Norman_C <Desource1990@yahoo.com>
2014-07-27 14:05:15 -0700

    Modified the cachematrix.R function.

--- cachematrix.R
+++ cachematrix.R
@@ -4,50 +4,48 @@
 ## searches for the inverse if computed, else it computes the inverse.
       
 
-makeCacheMatrix <- function(x = matrix()) {
-# initializing the inverse variable 'inv' to NULL
-      inv <- NULL
+CacheMatrix <- function(x = matrix()) {
+# initializing the inverse variable 'inverse' to NULL
+      inverse <- NULL
       set <- function(y){
       # this function is used to store the value of matrix in a different environment  
             x <<- y
-            inv <<- NULL
+            inverse <<- NULL
                     }
       get <- function() x
       # This function is used to obtain the value of the matrix
 
-      setinv <- function(solve) inv <<- solve
-      #   The setinverse function uses the inbuilt function 'solve' to determine 
+      setinverse <- function(solve) inverse <<- solve
+      #   The setinverse function uses the R function 'solve' to get 
       # the inverse of the matrix x
   
-      getinv <- function() inv
+      getinverse <- function() inverse
       #   The getinverse function is used to obtain the value of the inverse function
       # of the matrix
       
-      list(set = set, get = get, setinv = setinv, getinv = getinv)
+      list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
       }      
       
 
 ##   This function searches for the inverse of the matrix. If the inverse is found,
-## then the value is pulled out from the memory, else it is computed and stored in
-## the memory
+## then the value is retreived, otherwise it is computed and stored.
       
 
 cacheSolve <- function(x, ...){
-      inv <- x$getinv() 
+      inverse <- x$getinverse() 
       # searching the inverse of the function in different environment
-            if (!is.null(inv)){
+            if (!is.null(inverse)){
                   message("getting cached data")
-                  # if the inverse of the function is found then the inverse value will be printed   
-                  # from the memory
-                  return(inv)
+                  # if the inverse of the function is found then the inverse value will be printed
+                  inverse
             }
 
       data <- x$get()
       # computing the inverse of the matrix using the solve function
-      inv <- solve(data, ...)
-      # storing the inverse of the matrix in the memory
-      x$setinv(inv)
-      inv }
+      inverse <- solve(data, ...)
+      # storing the inverse of the matrix
+      x$setinverse(inverse)
+      inverse }
 
 
 
